a0072b111a37c1e5e5c8a43db6b8b4bcf131fb19
WynnieTeoh <135952962+WynnieTeoh@users.noreply.github.com>
2023-11-20 16:55:57 +1100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,183 @@
+let currentStoryIndex = 0;
+
+    const storyData = [
+      {
+        //text 0
+        text: "Let's test if you are truly Mable's friend",
+        choices: [
+          { text: "Okay, Let's do it!", nextIndex: 1 },
+          { text: "No, I don't want", nextIndex: 2 }
+        ]
+      },
+      {
+        //text 1
+        text: "Mable says: Aww~ I knew you care about me.",
+        choices: [
+          { text: "Why do you look a bit upset today?", nextIndex: 3 },
+          { text: "Do you know you have resting-bitch face?", nextIndex: 4 }
+        ]
+      },
+      {
+        //text 2
+        text: "Wahhh Cheebai.. You asshole.",
+        choices: [
+          { text: "Nahh~ I'm just kidding! Of course I'm your friend!", nextIndex: 5 },
+          { text: "That makes the two of us.", nextIndex: 6 }
+        ]
+      },
+      {
+        //text 3
+        text: "There are just too many things happened at once and I don't feel like talking about it now.",
+        choices: [
+          { text: "Mahai! Tell me now! ", nextIndex: 7 },
+          { text: "It's okay, you can tell me when you need someone to talk to.", nextIndex: 9 }
+        ]
+      },
+      {
+        //text 4
+        text: "Do you know you have lanchao mouth?",
+        choices: [
+          { text: "Woahh! Why you are so offensive today?", nextIndex: 5 },
+          { text: "You have pet pet eyes!", nextIndex: 6 }
+        ]
+      },
+      {
+        //text 5
+        text: "*sigh* I'm just not in the mood today",
+        choices: [
+          { text: "Wookay! Let me grab my popcorn.", nextIndex: 3 },
+          { text: "Do you want to talk about it?", nextIndex: 7 }
+        ]
+      },
+      {
+        //text 6
+        text: "You know what? I don't want to talk to you anymore. Bye!",
+        choices: [
+          { text: "Wait wait wait! I'm just kidding! Don't be mad.", nextIndex: 5 },
+          { text: "Okay Bye.", nextIndex: 8 }
+        ]
+      },
+      {
+        //text 7
+        text: "There are so many things I want to do, but so little time and I'm getting sleep deprivation.",
+        choices: [
+          { text: "Go to sleep now!.", nextIndex: 11 },
+          { text: "I sleep less than 2 hours a day", nextIndex: 12 }
+        ]
+      },
+      {
+        //text 8
+        text: "*Mable walks away* Congratulations, you are a horrible friend. Mable hates you.",
+        choices: []
+    
+      },
+      {
+        //text 9
+        text: "Aww~ Thank you. I'm glad I have a friend like you.",
+        choices: [
+          { text: "I'm joking, you can go fuck yourself.", nextIndex: 2 },
+          { text: "No worries! You can count on me", nextIndex: 10 }
+        ]
+      },
+      {
+        //text 10
+        text: "You are Mable's acquaintance.",
+        choices: []
+    
+      },
+      {
+        //text 11
+        text: "Wahh! Ordering me what to do. You think you're my dad?",
+        choices: [
+          { text: "You say you got not enough sleep ma, then go to sleep lah!", nextIndex: 13 },
+          { text: "What are the things that stop you from getting more sleep?", nextIndex: 14 }
+        ]
+      },
+      {
+        //text 12
+        text: "Are you even alive? I don't think I can do that",
+        choices: [
+          { text: "No, I'm dead. I'm a ghost haunting you now.", nextIndex: 15 },
+          { text: "I'm joking, I think you need to improve your time management", nextIndex: 16 }
+        ]
+      },
+      {
+        //text 13
+        text: "I can't! There are so many things I need to do!.",
+        choices: [
+          { text: "Do what?", nextIndex: 14 },
+          { text: "Fuck those things! Sleep is more important!", nextIndex: 15 }
+        ]
+      },
+      {
+        //text 14
+        text: "My job, Freelance, my side business, housechores and entertainment.",
+        choices: [
+          { text: "Just try to manage your time well", nextIndex: 16 },
+          { text: "Aiyoh, then how ah?", nextIndex: 17 }
+        ]
+      },
+      {
+        //text 15
+        text: "Fuck you lah! I don't want to talk to you anymore *Mable walks off* You are a horrible friend to Mable.",
+        choices: []
+      },
+      {
+        //text 16
+        text: "It's easier said than done.",
+        choices: [
+          { text: "It's okay! If you think you can! You can!", nextIndex: 18 },
+          { text: "Just fuck everything! YOLO and live the life you want!", nextIndex: 19 }
+        ]
+      },
+      {
+        //text 17
+        text: "I wish I know how.",
+        choices: [
+          { text: "You go to sleep now, maybe you will get the answer in your dream.", nextIndex: 18 },
+          { text: "I believe you will figure out someday", nextIndex: 18 }
+        ]
+      },
+      {
+        //text 18
+        text: "Thank you. I feel much better now *Mable smiles* You are a good friend to Mable",
+        choices: []
+      },
+      {
+        //text 19
+        text: "*Mable is speechless* You are a weird friend to Mable",
+        choices: []
+      },
+
+      // Add more story elements as needed
+    ];
+
+    function makeChoice(choiceIndex) {
+      const choice = storyData[currentStoryIndex].choices[choiceIndex - 1];
+
+      if (choice) {
+        currentStoryIndex = choice.nextIndex;
+        updateStory();
+      }
+    }
+
+    function updateStory() {
+      const storyElement = document.getElementById('story');
+      const buttonContainer = document.getElementById('button-container');
+
+      storyElement.textContent = storyData[currentStoryIndex].text;
+
+      // Clear existing buttons
+      buttonContainer.innerHTML = '';
+
+      // Create new buttons based on choices
+      storyData[currentStoryIndex].choices.forEach((choice, index) => {
+        const button = document.createElement('button');
+        button.textContent = choice.text; // Display the choice text dynamically
+        button.onclick = () => makeChoice(index + 1);
+        buttonContainer.appendChild(button);
+      });
+    }
+
+    // Initial rendering of the story
+    updateStory();
